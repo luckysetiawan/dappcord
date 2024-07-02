0a8f85c94a30b7f6c85d9e89ee12b183a8e4cfd1
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-02 16:40:14 +0700

    add create and get channel with tests

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -11,16 +11,20 @@ describe("Dappcord", function () {
   const NAME = "Dappcord";
   const SYMBOL = "DC";
 
+  beforeEach(async () => {
+    // Setup accounts
+    [ deployer, user ] = await ethers.getSigners();
+
+    // Deploy Contract
+    const DappcordFactory = await ethers.getContractFactory("Dappcord");
+    dappcord = await DappcordFactory.deploy(NAME, SYMBOL);
+
+    // Create a channel
+    const transaction = await dappcord.connect(deployer).createChannel("general", tokens(1));
+    await transaction.wait();
+  })
+
   describe("Deployment", () => {
-    beforeEach(async () => {
-      // Setup accounts
-      [ deployer, user ] = await ethers.getSigners();
-
-      // Deploy Contract
-      const DappcordFactory = await ethers.getContractFactory("Dappcord");
-      dappcord = await DappcordFactory.deploy(NAME, SYMBOL);
-    })
-
     it("Sets the name", async () => {
       let result = await dappcord.name();
       expect(result).to.equal(NAME);
@@ -34,6 +38,20 @@ describe("Dappcord", function () {
     it("Sets the owner", async () => {
       let result = await dappcord.owner();
       expect(result).to.equal(deployer.address);
+    })
+  })
+
+  describe("Creating Channels", () => {
+    it("Returns total channels", async () => {
+      let result = await dappcord.totalChannels();
+      expect(result).to.equal(1);
+    })
+
+    it("Returns channel attributes", async () => {
+      let channel = await dappcord.getChannel(1);
+      expect(channel.id).to.equal(1);
+      expect(channel.name).to.equal("general");
+      expect(channel.cost).to.equal(tokens(1));
     })
   })
 })
