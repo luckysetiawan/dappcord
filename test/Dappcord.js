544bd39f6e19b03e52764aa066593d07fba6fddf
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-02 16:13:17 +0700

    add ERC721 setup and tests

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,35 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let deployer, user;
+  let dappcord;
 
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  describe("Deployment", () => {
+    beforeEach(async () => {
+      // Setup accounts
+      [ deployer, user ] = await ethers.getSigners();
+
+      // Deploy Contract
+      const DappcordFactory = await ethers.getContractFactory("Dappcord");
+      dappcord = await DappcordFactory.deploy(NAME, SYMBOL);
+    })
+
+    it("Sets the name", async () => {
+      let result = await dappcord.name();
+      expect(result).to.equal(NAME);
+    })
+
+    it("Sets the symbol", async () => {
+      let result = await dappcord.symbol();
+      expect(result).to.equal(SYMBOL);
+    })
+
+    it("Sets the owner", async () => {
+      let result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
+    })
+  })
 })
