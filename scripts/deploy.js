763a05da915654f1cb5fc6d083869137be3decbb
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-02 17:51:22 +0700

    add deploy scripts and updated abis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,28 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup accounts & variables
+  const [ deployer ] = await ethers.getSigners();
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
 
+  // Deploy Contract
+  const DappcordFactory = await ethers.getContractFactory("Dappcord");
+  dappcord = await DappcordFactory.deploy(NAME, SYMBOL);
+  await dappcord.waitForDeployment();
+
+  console.log(`Deployed Dappcord Contract at: ${await dappcord.getAddress()}`);
+
+  // Create 3 Channels
+  const CHANNEL_NAMES = ["general", "intro", "jobs"];
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)];
+
+  for (let i = 0; i < 3; i++) {
+    const transaction = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i]);
+    await transaction.wait();
+
+    console.log(`Created text channel #${CHANNEL_NAMES[i]}`);
+  }
 }
 
 main().catch((error) => {
