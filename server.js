290b3f7f7e681e8fce4b01eb6146c15d0c24a901
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-03 15:53:50 +0700

    enhance config and seeding

--- server.js
+++ server.js
@@ -4,53 +4,66 @@ const app = express()
 const PORT = process.env.PORT || 3030
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}\n`))
 
-const messages = [
+const general = [
   {
     channel: "1",
-    account: "0xcA8Fa8f0b631EcdB18Cda619C4Fc9d197c8aFfCa",
-    text: "Welcome to Dappcord!"
-  },
-  {
-    channel: "2",
-    account: "0xcA8Fa8f0b631EcdB18Cda619C4Fc9d197c8aFfCa",
-    text: "Welcome to Dappcord everyone! My name is John and I've been a blockchain developer for 2+ years."
+    account: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    text: "First :)"
   },
   {
     channel: "1",
-    account: "0x1b3cB81E51011b549d78bf720b0d924ac763A7C2",
+    account: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
     text: "Hello everyone!"
   },
   {
-    channel: "2",
-    account: "0x1b3cB81E51011b549d78bf720b0d924ac763A7C2",
-    text: "Hey there! My name is Ann and I'm an aspiring blockchain developer!"
+    channel: "1",
+    account: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    text: "Welcome, welcome!"
   },
   {
     channel: "1",
-    account: "0x701C484bfb40ac628aFA487b6082f084B14AF0BD",
-    text: "Hey everyone!"
+    account: "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+    text: "Hi guys, does anyone here know how to automatically mint an NFT?"
   },
   {
     channel: "1",
-    account: "0x189B9cBd4AfF470aF2C0102f365FC1823d857965",
-    text: "Hey there, great to be here!"
+    account: "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+    text: "NFT? I'd assume you'll use ERC721"
   },
   {
     channel: "1",
-    account: "0x176F3DAb24a159341c0509bB36B833E7fdd0a132",
-    text: "Hope everyone is having a good day ;)"
+    account: "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+    text: "Send me a DM, I'll help you out :D"
   },
   {
     channel: "1",
-    account: "0x828103B231B39ffFCe028562412B3c04A4640e64",
-    text: "Hello!"
-  },
-  {
-    channel: "1",
-    account: "0x176F3DAb24a159341c0509bB36B833E7fdd0a132",
-    text: "Does anyone have any tips on becoming a blockchain developer?"
-  },
+    account: "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65",
+    text: "Guys, what kind of code editor should I use for solidity? I'm kinda new"
+  }
 ]
+
+const intro = [
+  {
+    channel: "2",
+    account: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    text: "Hi! My name is John and I'm a blockchain engineer for about 2+ years"
+  },
+  {
+    channel: "2",
+    account: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+    text: "Hey there! I'm Ann and I want to be a blockchain developer"
+  }
+]
+
+const jobs = [
+  {
+    channel: "3",
+    account: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    text: "Looking for someone that interested in blockchain dev career, comp-sci graduate, 1+ year software engineer, and have working knowledge of Crypto, Ethereum, and Solidity. Send me a DM!"
+  }
+]
+
+const messages = general.concat(intro, jobs);
 
 const { Server } = require("socket.io");
 const io = new Server(server, {
